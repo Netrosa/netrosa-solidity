7fdef6ea2ba2c11219406922a411b4c34af0f827
Steven Landers <steven.landers@gmail.com>
2018-11-06 19:37:33 -0500

    adding creation lambda

---
+++ lambdas/create-form.js
@@ -0,0 +1,63 @@
+'use strict';
+const iopipe = require('@iopipe/iopipe')({ token: process.env.IO_PIPE_TOKEN });
+const networks = require("./lib/eth-networks");
+const encryption = require("./lib/encryption")
+const forms = require("./lib/forms")
+
+module.exports.createForm = iopipe(async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  let network = event.network;
+  let company = event.company;
+  let formId = event.formId;
+
+  if(!company){
+    throw new Error("company is required")
+  }
+  if(!formId){
+    throw new Error("formId is required")
+  }
+  if(!network){
+    throw new Error("network is required")
+  }
+
+  let nv = await networks.NetvoteProvider(event.network);
+  let version = nv.version();
+  let submissionLog = await nv.SubmissionLog(version);
+
+  let hashedFormId = nv.sha3(event.formId);
+  let continuousReveal = event.continuousReveal || false;
+  let transactor = nv.gatewayAddress();
+
+  await encryption.generateKeys(formId);
+
+  let publicKey = ""
+  if(continuousReveal){
+    publicKey = await encryption.getDecryptedKey(formId, "encryption")
+  }
+
+  // duplicate, fast succeed to avoid blowing gas
+  let exists = await submissionLog.formExists(hashedFormId);
+  if(exists){
+    callback(null, {formId: hashedFormId})
+    return;
+  }
+
+  try{
+    let nonce = await nv.Nonce();
+    let tx = await submissionLog.createForm(transactor, publicKey, hashedFormId, {from: transactor, nonce: nonce})
+    console.log(tx);
+    
+    let txHash = tx.tx;    
+    await forms.setFormSuccess(company, formId, version, txHash);
+
+    callback(null, {formId: hashedFormId})
+
+  } catch(e){
+    console.error(e);
+    await forms.setFormError(company, formId, e.message);
+
+    //not repeating because don't want to run out of ether
+    callback(null, {error: e.message})
+  }
+});
